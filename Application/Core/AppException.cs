a89fc68abf1af41b9b16fd9dc35ad98933eb1b31
Chinara Guliyeva <ch.quliyeva@dxa.gov.az>
2025-09-26 15:11:51 +0400

    Finished validation

---
+++ Application/Core/AppException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Core
+{
+    public class AppException(int statusCode, string messge, string? details)
+    {
+        public int StatusCode { get; set; } = statusCode;
+        public string Message { get; set; } = messge;
+
+        public string? Details { get; set; } = details;
+        
+    }
+}
